4db4c6ab90503580087b558bdaa673664c9b6900
AbraaoMota <abraaomot@outlook.com>
2015-12-23 18:41:40 +0000

    added the scanner

---
+++ htmlScanner.rb
@@ -0,0 +1,129 @@
+require 'net/http'
+require 'net/https'
+require 'open-uri'
+require 'nokogiri'
+require 'openssl'
+require 'fileutils'
+
+
+$minimumURLLength = 6
+$HTTPSPortNum = 443
+$frontPageSize = 70
+
+
+def grab_html
+	# Defining url
+	startURL = 'https://www.reddit.com/r/wallpapers'
+	url = startURL
+	url = URI.parse(url)
+
+	# Establishing Connection
+	puts "Establishing connection to " + startURL + "...."
+
+	# Ensures https security
+	http = Net::HTTP.new(url.host, url.port)
+	http.use_ssl = true if url.port == $HTTPSPortNum
+	http.verify_mode = OpenSSL::SSL::VERIFY_NONE if url.port == $HTTPSPortNum
+
+
+	path = url.path
+	path += "?" + url.query unless url.query.nil?
+	res, data = http.get( path )
+	case res
+	  when Net::HTTPSuccess, Net::HTTPRedirection
+	    # parse link
+	    html = open(url)
+			doc = Nokogiri::HTML(html)
+	    
+			# Saves html into xml file
+			puts "Loading html from " + startURL + " into htmlFile ..."
+			htmlFile = File.open('html', 'w')
+			File.write(htmlFile, doc)
+			return htmlFile 
+	  else
+	    return "failed" + res.to_s
+	end
+end
+
+
+def parse_html (htmlFile)
+inUserSubmissionSection = false
+fileLinks = Array.new($frontPageSize) {String.new}
+divCounter = 0
+puts 'Parsing wallpaper links....'
+
+	File.open(htmlFile, "r") do |file_handle|
+	  file_handle.each_line do |currentLine|
+	    # if seeing the siteTable div with linklisting, then looking at
+			# the user submissions in the webpage
+			if currentLine.include? '<div id="siteTable" class="sitetable linklisting">'
+				inUserSubmissionSection = true;
+				divCounter = 1
+			end
+
+			if inUserSubmissionSection 
+				str = findImages(currentLine)
+				if (str.length > $minimumURLLength)
+					#puts "This is added" + str
+					fileLinks.push(str)
+				end
+
+				if (currentLine.include? "<div")
+					divCounter = divCounter + 1
+				end
+			
+				if (currentLine.include? "</div>")
+					divCounter = divCounter - 1
+				end
+
+				if divCounter == 0 
+					break
+				end
+			end
+	  end
+	end
+  return fileLinks
+end
+
+def findImages(currentLine)
+
+	sizeOfHrefStr = 5
+	if currentLine.include? '<p class="title">'
+		hrefIndex = currentLine.index('href="')
+		firstSpeechMarkIndex = hrefIndex + sizeOfHrefStr
+		secondSpeechMarkIndex = currentLine.index('" tabindex="1"')
+		str = currentLine[firstSpeechMarkIndex+1..secondSpeechMarkIndex-1] 	
+		return str 	
+	end
+	return ""
+end
+
+
+def downloadImages(fileLinks)
+	# Make new subdir
+	puts 'Making directory for pictures...'
+	FileUtils.mkdir_p("#{File.expand_path(File.dirname(__FILE__))}/pics/")
+
+
+
+	# Counter for the picture
+	picCounter = 1
+	
+	puts 'Downloading images...'
+
+	# Loop through the links of the files, download them into new subdir
+	fileLinks.each{ |fileLink|
+		if (fileLink.include? 'http')
+			File.open("#{File.expand_path(File.dirname(__FILE__))}/pics/#{picCounter}.jpg", 'wb') do |fo|
+			  fo.write open(fileLink.to_s).read 
+			  picCounter += 1	
+			end
+		end
+ 	}
+end
+
+
+# execute it all
+file = grab_html
+links = parse_html(file)
+downloadImages(links)
